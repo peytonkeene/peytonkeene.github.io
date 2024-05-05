9eba7e6356cbeac9742b7e211bea2e9517d2eab3
peytonkeene <32674415+peytonkeene@users.noreply.github.com>
2024-05-04 23:20:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
    function generateNarrative() {
-    var narrative = "";
 
     // Dispatch section
     narrative += "Dispatch: ";
